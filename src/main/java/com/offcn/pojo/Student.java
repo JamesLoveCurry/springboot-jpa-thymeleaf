6616d2e7a7cdedfffc31a80b2edf7d5e804936e5
JamesLoveCurry <1107427228@qq.com>
2020-02-01 13:38:19 +0800

    添加学生类

---
+++ src/main/java/com/offcn/pojo/Student.java
@@ -0,0 +1,33 @@
+package com.offcn.pojo;
+
+/**
+ * @author
+ * @create 2020-02-01 12:51
+ */
+public class Student {
+    /**
+     *学生学号
+     */
+     private Integer id;
+     /**
+      *学生姓名
+      */
+      private String name;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
